08d3a5c78241985c651d4f006085f73338d595a0
ozantalay <ozntalay@hotmail.com>
2024-09-27 15:24:20 +0300

    baslangic

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  return <WordByWord />;
+}
+
+const WordByWord = () => {
+  // KODUNUZ BURAYA GELECEK
+};
+
+export default App;
