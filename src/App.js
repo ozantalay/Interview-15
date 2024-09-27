69cb8fdfac8a0df654b9b62ce3b913844f9a06ec
ozantalay <ozntalay@hotmail.com>
2024-09-27 16:04:48 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -6,6 +6,41 @@ function App() {
 
 const WordByWord = () => {
   // KODUNUZ BURAYA GELECEK
+  const[inputText,setInputText]=useState('')
+  const[displayText,setDisplayText]=useState('')
+  const [wordIndex,setWordIndex]=useState(0)
+  const[wordArray, setWordArray]=useState([])
+
+  const handleInputChange=(e)=>{
+    setInputText(e.target.value)
+    setWordIndex(0)
+    setDisplayText('')
+    setWordArray(e.target.value.split(' '))
+
+  }
+  useEffect(()=>{
+    if(wordIndex<wordArray.length){
+     const interval=setInterval(() => {
+        
+       setDisplayText((prev)=>prev+" "+wordArray[wordIndex])
+       setWordIndex((prev)=>prev+1)
+      }, 500);
+      return()=>clearInterval(interval)
+    }
+  } ,[wordIndex,wordArray])
+
+    return (
+      <div className="text-center">
+        <input
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder="Metin giriniz"
+          className="border-[1px] border-black pl-1"
+        />
+        <div>{displayText}</div>
+      </div>
+    );
 };
 
 export default App;
